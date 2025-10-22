18b547085b82dba93adcf5f2bdc43060bad2cd19
xxx_Ape_xxx <35788446+Monkeybrain73@users.noreply.github.com>
2025-10-22 17:18:49 +0200

    Add project files.

---
+++ boattags/boattagsModSystem.cs
@@ -0,0 +1,29 @@
+﻿using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+
+namespace boattags
+{
+    public class boattagsModSystem : ModSystem
+    {
+
+        // Called on server and client
+        // Useful for registering block/entity classes on both sides
+        public override void Start(ICoreAPI api)
+        {
+            api.Logger.Notification("Hello from template mod: " + api.Side);
+        }
+
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+            api.Logger.Notification("Hello from template mod server side: " + Lang.Get("boattags:hello"));
+        }
+
+        public override void StartClientSide(ICoreClientAPI api)
+        {
+            api.Logger.Notification("Hello from template mod client side: " + Lang.Get("boattags:hello"));
+        }
+
+    }
+}
